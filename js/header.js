ed41656cdc734105ab5e27fa791e588caaa45f95
JasryEgbowon <Jasry.egbowon@gmail.com>
2025-08-10 13:29:02 +0100

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,20 @@
+export const headerHTML = `
+<header>
+  <div class="logo">Silque Fashion House</div>
+  <nav>
+    <ul>
+      <li class="dropdown">
+        <a href="#" class="dropbtn">Collections ▾</a>
+        <ul class="dropdown-content">
+          <li><a href="corporate.html">Corporate</a></li>
+          <li><a href="formal.html">Formal</a></li>
+          <li><a href="traditional.html">Traditional</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Board of Trustees</a></li>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+`;
