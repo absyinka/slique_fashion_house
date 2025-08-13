2377ba35220157b928d4234f5bff28f89dbc898b
JasryEgbowon <Jasry.egbowon@gmail.com>
2025-08-13 08:58:56 +0100

    Update footer.js
    added links

--- js/footer.js
+++ js/footer.js
@@ -1,11 +1,11 @@
 export const footerHTML = `
 <footer>
   <div class="footer-section about">
-    <h3>About Us</h3>
+    <a href="about_us.html">About us</a> 
     <p>Silque Fashion House represents elegance, tradition, and innovation in fashion. We deliver unique styles to make bold statements.</p>
   </div>
   <div class="footer-section contact">
-    <h3>Contact</h3>
+    <h3><a href="contact_us.html">Contact</a> </h3>
     <p>Email: contact@silquefashion.com</p>
     <p>Phone: +123 456 7890</p>
     <p>Location: Lagos, Nigeria</p>
