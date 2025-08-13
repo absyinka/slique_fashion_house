26ff7402d02bd303e5cd1f15c514cd5de1ef95e0
JasryEgbowon <Jasry.egbowon@gmail.com>
2025-08-13 08:59:15 +0100

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,19 +1,21 @@
 export const headerHTML = `
 <header>
-  <div class="logo">Silque Fashion House</div>
+  <div class="logo">
+    <a href="index.html">Silque Fashion House</a>
+  </div>
   <nav>
     <ul>
       <li class="dropdown">
-        <a href="#" class="dropbtn">Collections ▾</a>
+        <a href="collections.html" class="dropbtn">Collections ▾</a>
         <ul class="dropdown-content">
           <li><a href="corporate.html">Corporate</a></li>
           <li><a href="formal.html">Formal</a></li>
           <li><a href="traditional.html">Traditional</a></li>
         </ul>
       </li>
-      <li><a href="#">Board of Trustees</a></li>
-      <li><a href="#">About Us</a></li>
-      <li><a href="#">Contact</a></li>
+      <li><a href="board_of_trustees.html">Board of Trustees</a></li>
+      <li><a href="about_us.html">About Us</a></li>
+      <li><a href="contact_us.html">Contact</a></li>
     </ul>
   </nav>
 </header>
